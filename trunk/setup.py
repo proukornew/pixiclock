8e4f6b6090c9bdc43fd9172aa9e5d5841c30c72b
a.michurin <a.michurin@1b811fce-8b11-1e89-de16-e64446d21e51>
2010-04-19 19:21:06 +0000

    version 0.3.0; network mode (experimental), command line options "hvp:", demos and examples, todo list

--- trunk/setup.py
+++ trunk/setup.py
@@ -7,7 +7,7 @@ except Exception, e:
     exit(1)
 
 setup(name       = 'pixiclock',
-      version    = '0.2.0',
+      version    = '0.3.0',
       author     = 'Alexey Michurin',
       author_email = 'a.michurin@gmail.com',
       maintainer = 'Alexey Michurin',
