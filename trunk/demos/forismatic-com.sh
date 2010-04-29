9a49af6686c442de82faa489a1769e22b094b59d
a.michurin <a.michurin@1b811fce-8b11-1e89-de16-e64446d21e51>
2010-04-29 02:11:21 +0000

    0.4.0; updates, fixes. release.

--- trunk/demos/forismatic-com.sh
+++ trunk/demos/forismatic-com.sh
@@ -1,24 +1,41 @@
 #!/bin/sh
 
+
 # Copyright (c) 2010 Alexey Michurin <a.michurin@gmail.com>
 
-# usage:
-# pixiclock -c /full_path_to/forismatic-com.sh
+# usage (pipe mode):
+# pixiclock -c /path/forismatic-com.sh
+#
+# or (network mode):
+# pixiclock -n
+# forismatic-com.sh
 
+client=pixiclock-client # or client='python ../pixiclock-client'
 lang=en # or lang=ru
+
+if test "a$PIXICLOCK" != 'a'
+then
+  client=cat
+fi
 
 while :
 do
+  (
   n=`date +%s`
   n=$(($n%1000000))
   wget -qO- --post-data='method=getQuote&key='$n'&format=text&lang='$lang \
     http://api.forismatic.com/api/1.0/ | fmt -w 40
   echo "($n)"
-  echo 'BG=#000000'
-  for i in 0 1 2 3 4 5 6 7 8 9 a b c d e f
+  echo 'BG=#444444'
+  a=''
+  b=''
+  for i in 4 5 6 7 8 9 a b c d
   do
-    echo "FG=#$i$i$i$i$i$i;DELAY=100"
+    c="FG=#$i$i$i$i$i$i;DELAY=100;"
+    a="$a$c"
+    b="$c$b"
   done
-  echo 'DELAY=10000'
-  sleep 1000
+  echo "${a}DELAY=10000;$b"
+  ) | $client
+  sleep 100
 done
