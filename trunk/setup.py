d8dc006d4bc1359f447b8b8157875a5f89d8c7e3
a.michurin <a.michurin@1b811fce-8b11-1e89-de16-e64446d21e51>
2010-06-08 01:10:31 +0000

    fix error messages; add commands BD, BDWIDTH, MARGIN; update README and some examples

--- trunk/setup.py
+++ trunk/setup.py
@@ -7,7 +7,7 @@ except Exception, e:
     exit(1)
 
 setup(name       = 'pixiclock',
-      version    = '0.5.4',
+      version    = '0.5.5',
       author     = 'Alexey Michurin',
       author_email = 'a.michurin@gmail.com',
       maintainer = 'Alexey Michurin',
