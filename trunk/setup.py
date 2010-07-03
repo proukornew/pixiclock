48676f8221e74c18195f233d4c0208c9bcbc2a6a
a.michurin <a.michurin@1b811fce-8b11-1e89-de16-e64446d21e51>
2010-07-03 19:02:22 +0000

    0.5.8: check whether messages are properly encoded in UTF-8

--- trunk/setup.py
+++ trunk/setup.py
@@ -7,7 +7,7 @@ except Exception, e:
     exit(1)
 
 setup(name       = 'pixiclock',
-      version    = '0.5.7',
+      version    = '0.5.8',
       author     = 'Alexey Michurin',
       author_email = 'a.michurin@gmail.com',
       maintainer = 'Alexey Michurin',
